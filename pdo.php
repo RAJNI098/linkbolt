5f4d51550358a54bbc61c0dabf3c6199de220508
Fardeen The Developer <fardeenthedeveloper@gmail.com>
2026-02-14 11:46:43 +0530

    db config without host for security

--- pdo.php
+++ pdo.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db   = 'link_bolt';
-$user = 'root';
-$pass = '';
+$host = '';
+$db   = 'u214833083_linkBolt';
+$user = 'u214833083_fardeen3';
+$pass = 'huu_7G776y767iHY76THy65gh8H886Hj';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
